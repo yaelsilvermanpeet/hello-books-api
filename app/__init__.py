4384195c52b340a82782e24e1a315283b7a404aa
Yael Peet <yaelpeet@gmail.com>
2023-05-09 07:48:00 -0700

    book model, routes, test files made

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from app import db
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes.book_routes import books_bp
+    app.register_blueprint(books_bp)
+
+    return app
---
+++ app/routes/book_routes.py
@@ -0,0 +1,19 @@
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request, abort
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET", "POST"])
+def handle_books():
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
